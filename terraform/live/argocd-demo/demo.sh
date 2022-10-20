836781b35948af3fc8878e8ef9763a8123d94be4
Marek <xbox360.rozek@gmail.com>
2022-10-20 17:14:35 +0200

    feat: remove terraform files from state

--- terraform/live/argocd-demo/demo.sh
+++ terraform/live/argocd-demo/demo.sh
@@ -3,10 +3,10 @@
 set -e
 # Port-forward to argocd
 
-kubectl port-forward svc/argocd-server 5000:80 1>/dev/null &
+kubectl port-forward svc/argocd-server -n argocd 5000:80 1>/dev/null &
 
 open -a "Google Chrome" http://localhost:5000
 
 # Get argocd initial secret
 
-kubectl get secrets argocd-initial-admin-secret -o jsonpath={.data.password}| base64 -d | pbcopy .
+kubectl get secrets argocd-initial-admin-secret -n argocd -o jsonpath={.data.password}| base64 -d | pbcopy .
--- terraform/live/argocd-demo/main.tf
+++ terraform/live/argocd-demo/main.tf
@@ -8,5 +8,6 @@ module "argocd" {
   cluster_client_certificate = module.kind.kind_client_certificate
   cluster_client_key         = module.kind.kind_client_key
   cluster_host               = module.kind.kind_endpoint
-  argocd_root_configuration  = "/argocd/apps/staging"
+  argocd_configuration_git_repository = "https://github.com/rozek1997/gitops-local-demo"
+  argocd_root_configuration_path = "argocd/apps/staging"
 }
